8412c5e783258b6c8b7ae154abd7ff1adf8de00b
Jonas Weber <b30253342@gmail.com>
2026-09-08 09:20:58 +0200

    centralization of global application settings

--- constants.py
+++ constants.py
@@ -1,19 +1,34 @@
-from typing import Tuple
+import platform
 
-# General constants used in the autoclicker application
-MAX_CLICKS: int = 1000  # Maximum number of clicks allowed
-CLICK_DELAY: float = 0.1  # Delay between clicks in seconds
-DEFAULT_CLICK_POSITION: Tuple[int, int] = (100, 200)  # Default position to click
+# Application configuration constants
+APP_NAME = "python-utils-35"
+VERSION = "1.0.0"
 
-# Hotkey constants for starting and stopping the autoclicker
-START_HOTKEY: str = 'ctrl+shift+s'  # Hotkey to start the autoclicker
-STOP_HOTKEY: str = 'ctrl+shift+x'  # Hotkey to stop the autoclicker
+# Mouse action constants
+LEFT_BUTTON = "left"
+RIGHT_BUTTON = "right"
+MIDDLE_BUTTON = "middle"
 
-# Possible states of the autoclicker
-class ClickerState:
-    NOT_RUNNING = 0  # Autoclicker is not running
-    RUNNING = 1      # Autoclicker is currently running
-    STOPPED = 2      # Autoclicker has been stopped
+# Default operation timings
+DEFAULT_INTERVAL = 0.1
+MAX_CLICK_RATE_LIMIT = 0.001
 
-# Other configuration constants
-WINDOW_TITLE: str = 'Autoclicker'  # Title of the autoclicker window
+# System specific pathing and configurations
+IS_WINDOWS = platform.system() == "Windows"
+IS_LINUX = platform.system() == "Linux"
+IS_MACOS = platform.system() == "Darwin"
+
+# Error and log patterns
+DEFAULT_LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+LOG_FILE_NAME = "autoclicker.log"
+
+# User interaction constants
+DEFAULT_HOTKEY = "f6"
+STOP_HOTKEY = "f7"
+
+def get_supported_buttons():
+    """Returns list of supported mouse buttons."""
+    return [LEFT_BUTTON, RIGHT_BUTTON, MIDDLE_BUTTON]
+
+if __name__ == "__main__":
+    print(f"Loaded {APP_NAME} constants for {platform.system()}")
